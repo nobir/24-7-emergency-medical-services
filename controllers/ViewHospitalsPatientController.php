36c7244624e24ed9d870c2ff0d42bb65d70dc360
Khukomoni <khukomoni163@gmail.com>
2021-12-16 00:11:58 +0600

    [Added] View Hospital

---
+++ controllers/ViewHospitalsPatientController.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Can access direcly by URL
+ */
+
+define("_DIRECT_ACCESS", true);
+
+require_once dirname(dirname(__FILE__)) . "/helper/functions.php";
+require_once _ROOT_DIR . "controllers/ValidationController.php";
+
+if (!_get_is_logged_in()) {
+    header("Location: " . _get_url("login.php"));
+    exit();
+}
+
+if (_get_session_val('utype') !== "patient") {
+    header("Location: " . _get_url("dashboard.php"));
+    exit();
+}
+
+if (isset($_POST['viewhospitals'])) {
+    // _var_dump($_POST);
+    // return;
+
+    $has_err = false;
+
+    $messages = [
+        "unsuccess" => "",
+        "success" => "",
+        "errors" => [],
+        "data" => []
+    ];
+
+    $name = "";
+    $email = "";
+
+    if (isset($_POST['name'])) {
+        $name = $_POST['name'];
+        $messages['data']['name'] = $name;
+    }
+
+    // Email
+    _validate_email_viewuser($email, $_POST['email'], $messages, $has_err);
+
+    // _var_dump($email);
+    // _var_dump($name, $email);
+    // exit();
+
+    if (!$has_err) {
+        require_once _ROOT_DIR . "models/Patient/PatientModel.php";
+
+        $messages['data']['hospitals'] = PatientModel::getAllHospitals($name, $email);
+
+        if (count($messages['data']['hospitals']) === 0) {
+            $messages['errors']['hospitals'] = false;
+        }
+
+        _set_session_val("messages", $messages);
+        header("Location: " . _get_url("users/patient/view-hospitals.php"));
+    } else {
+        _set_session_val("messages", $messages);
+        header("Location: " . _get_url("users/patient/view-hospitals.php"));
+        exit();
+    }
+} else {
+    header("Location: " . _get_url("users/patient/view-hospitals.php"));
+    exit();
+}
--- users/patient/view-hospitals.php
+++ users/patient/view-hospitals.php
@@ -22,6 +22,22 @@ if (_get_session_val('utype') != "patient") {
     exit();
 }
 
+$hospitals = [];
+
+if (_get_messages_data('hospitals') !== false) {
+    $hospitals = _get_messages_data('hospitals');
+} else {
+    $messages = _get_messages();
+    $messages['data']['hospitals'] = false;
+    _set_session_val("messages", $messages);
+}
+
+if (_get_messages_data('hospitals') === false) {
+    require_once _ROOT_DIR . "models/Patient/PatientModel.php";
+
+    $hospitals = PatientModel::getAllHospitals();
+}
+
 ?>
 
 <?php header_section("EMS | View Hospitals"); ?>
@@ -41,6 +57,73 @@ if (_get_session_val('utype') != "patient") {
             <?php if (_get_is_logged_in()) side_menu(); ?>
 
             <div class="col-md-<?php echo _get_is_logged_in() ? "9" : "12"; ?>">
+
+                <form id="view-hospital-form" action="<?php echo _get_url("controllers/ViewHospitalsPatientController.php"); ?>" method="POST" class="need-validation">
+                    <div class="row">
+                        <div class="col-12 col-md-4 mb-3">
+                            <input type="text" name="name" class="form-control" value="<?php echo _get_messages_data('name'); ?>" placeholder="Name...">
+                        </div>
+                        <div class="col-12 col-md-4 mb-3 has-validation">
+                            <input type="text" name="email" class="form-control<?php echo _get_messages_css_class_name('email'); ?>" value="<?php echo _get_messages_data('email'); ?>" placeholder="Email..">
+
+                            <?php if (_get_messages_errors('email')) : ?><div class="invalid-feedback"><?php echo _get_messages_errors('email'); ?></div><?php endif; ?>
+                        </div>
+                        <div class="col-12 col-md-4 mb-3">
+                            <input type="submit" name="viewhospitals" class="form-control btn btn-success" value="Search">
+                        </div>
+                    </div>
+                </form>
+
+                <hr>
+
+                <div class="d-flex justify-content-center align-items-center">
+                    <div class="table-responsive">
+                        <table class="table table-<?php echo _CONFIG['THEME_COLOR']; ?> table-striped">
+
+                            <?php if (count($hospitals) > 0) : ?>
+
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Phone</th>
+                                        <th scope="col">Area</th>
+                                        <th scope="col">Sub District</th>
+                                        <th scope="col">District</th>
+                                        <th scope="col">Division</th>
+                                    </tr>
+                                </thead>
+
+                            <?php endif; ?>
+
+                            <tbody>
+
+                                <?php foreach ($hospitals as $hospital) : ?>
+
+                                    <tr>
+                                        <td><?php echo $hospital['h_name']; ?></td>
+                                        <td><?php echo $hospital['h_email']; ?></td>
+                                        <td><?php echo $hospital['h_phone']; ?></td>
+                                        <td><?php echo $hospital['h_area']; ?></td>
+                                        <td><?php echo $hospital['h_subdistrict']; ?></td>
+                                        <td><?php echo $hospital['h_district']; ?></td>
+                                        <td><?php echo $hospital['h_division']; ?></td>
+                                    </tr>
+
+                                <?php endforeach; ?>
+
+                                <?php if (count($hospitals) == 0) : ?>
+
+                                    <tr class="text-center">
+                                        <td colspan="6">No Hospitals Found</td>
+                                    </tr>
+
+                                <?php endif; ?>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
 
             </div>
 
